cd6f45fb9ee50d02ccaf323a3e019faa09fea760
Unknown <ghozam.alghifari96@gmail.com>
2017-10-28 18:23:36 +0700

    Aplikasi Thasya Busana Sample

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,210 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/apple-icon.png') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+        <title>War-Mart.id</title>
+
+      <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Bootstrap core CSS     -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
+    <!--  Material Dashboard CSS    -->
+    <link href="{{ asset('css/material-dashboard.css?v=1.2.0') }}" rel="stylesheet" />
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+
+     <link href="{{ asset('css/selectize.bootstrap3.css') }}" rel="stylesheet">
+
+     
+    <link href="{{ asset('css/bootstrap-datepicker.min.css') }}" rel="stylesheet">
+     
+    <!--     Fonts and icons     -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+  <style type="text/css">
+    
+    .table>thead>tr>th, .table>tbody>tr>th, .table>tfoot>tr>th, .table>thead>tr>td, .table>tbody>tr>td, .table>tfoot>tr>td{
+    padding: 1px;
+    }
+  </style>
+
+<body>
+    <div class="wrapper">
+        <div class="sidebar" data-active-color="rose" data-background-color="black" data-image="{{ asset('img/sidebar-1.jpg') }}">
+            <!--
+        Tip 1: You can change the color of active element of the sidebar using: data-active-color="purple | blue | green | orange | red | rose"
+        Tip 2: you can also add an image using data-image tag
+        Tip 3: you can change the color of the sidebar with data-background-color="white | black"
+    -->
+            <div class="logo">
+                <a href="https://war-mart.id" class="simple-text logo-mini">
+                    WM
+                </a>
+                <a href="https://war-mart.id" class="simple-text logo-normal">
+                    WAR-MART.ID
+                </a>
+            </div>
+            <div class="sidebar-wrapper">
+              
+                <ul class="nav">
+                    <li>
+                        <a data-toggle="collapse" href="#logout">
+                            <i class="material-icons">person</i>
+                            <p>{{ Auth::user()->name }}
+                                <b class="caret"></b>
+                            </p>
+                        </a> 
+                        <div class="collapse" id="logout">
+                            <ul class="nav">
+                                    <li>
+                                         <a href="{{ url('/ubah-profil') }}">Ubah Profil</a>
+                                    </li>
+                                    <li>
+                                         <a href="{{ url('/ubah-password') }}">Ubah Password</a>
+                                    </li>
+                                    <li>
+                                         <a href="{{ url('/logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+
+                                    </li>     
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="active">
+                        <a href="{{ url('/')}}">
+                            <i class="material-icons">dashboard</i>
+                            <p>Dashboard</p>
+                        </a>
+                    </li>
+ 
+                 
+                </ul>
+            </div>
+        </div>
+        <div class="main-panel">
+            <nav class="navbar navbar-transparent navbar-absolute">
+                <div class="container-fluid">
+                    <div class="navbar-minimize">
+                        <button id="minimizeSidebar" class="btn btn-round btn-white btn-fill btn-just-icon">
+                            <i class="material-icons visible-on-sidebar-regular">more_vert</i>
+                            <i class="material-icons visible-on-sidebar-mini">view_list</i>
+                        </button>
+                    </div>
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#"> Dashboard </a>
+                    </div>
+                    <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav navbar-right"> 
+                       
+                           
+                        </ul>
+                     
+                    </div>
+                </div>
+            </nav>
+            <div class="content">
+                <div class="container-fluid">
+
+                       <div class="row">
+                     @include('layouts._flash')
+                    @yield('content')
+                    </div>
+              
+                </div>
+                <!-- end container fluid -->
+            </div>
+            <footer class="footer">
+                <div class="container-fluid">
+                    <nav class="pull-left">
+          
+                    </nav>
+                    <p class="copyright pull-right">
+                        &copy;
+                        <script>
+                            document.write(new Date().getFullYear())
+                        </script>
+                        <a href="https://andaglos.id">PT Andaglos Global Teknologi</a>, made with love for a better web
+                    </p>
+                </div>
+            </footer>
+        </div>
+    </div>
+</body>
+<!--   Core JS Files   -->
+<script src="{{ asset('js/jquery-3.2.1.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/material.min.js') }}" type="text/javascript"></script>
+
+<script src="{{ asset('js/bootstrap-datepicker.min.js') }}"></script>
+<script src="{{ asset('js/perfect-scrollbar.jquery.min.js') }}" type="text/javascript"></script>
+<!-- Library for adding dinamically elements -->
+<script src="{{ asset('js/arrive.min.js') }}" type="text/javascript"></script>
+<!-- Forms Validations Plugin -->
+<script src="{{ asset('js/jquery.validate.min.js') }}"></script>
+<!-- Promise Library for SweetAlert2 working on IE -->
+<script src="{{ asset('js/es6-promise-auto.min.js') }}"></script>
+<!--  Plugin for Date Time Picker and Full Calendar Plugin-->
+<script src="{{ asset('js/moment.min.js') }}"></script>
+<!--  Charts Plugin, full documentation here: https://gionkunz.github.io/chartist-js/ -->
+<script src="{{ asset('js/chartist.min.js') }}"></script>
+<!--  Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
+<script src="{{ asset('js/jquery.bootstrap-wizard.js') }}"></script>
+<!--  Notifications Plugin, full documentation here: http://bootstrap-notify.remabledesigns.com/    -->
+<script src="{{ asset('js/bootstrap-notify.js') }}"></script>
+<!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
+<script src="{{ asset('js/bootstrap-datetimepicker.js') }}"></script>
+<!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
+<script src="{{ asset('js/jquery-jvectormap.js') }}"></script>
+<!-- Sliders Plugin, full documentation here: https://refreshless.com/nouislider/ -->
+<script src="{{ asset('js/nouislider.min.js') }}"></script>
+<!--  Google Maps Plugin    -->
+<!-- <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> -->
+<!--  Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+<script src="{{ asset('js/jquery.select-bootstrap.js') }}"></script>
+<!--  DataTables.net Plugin, full documentation here: https://datatables.net/    -->
+<script src="{{ asset('js/jquery.dataTables.js') }}"></script>
+<!-- Sweet Alert 2 plugin, full documentation here: https://limonte.github.io/sweetalert2/ -->
+<script src="{{ asset('js/sweetalert2.js') }}"></script>
+<!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+<script src="{{ asset('js/jasny-bootstrap.min.js') }}"></script>
+<!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
+<script src="{{ asset('js/fullcalendar.min.js') }}"></script>
+<!-- Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
+<script src="{{ asset('js/jquery.tagsinput.js') }}"></script>
+<!-- Material Dashboard javascript methods -->
+<script src="{{ asset('js/material-dashboard.js?v=1.2.0') }}"></script>
+<!-- Material Dashboard DEMO methods, don't include it in your project! -->
+<script src="{{ asset('js/demo.js') }}"></script>
+
+<script src="{{ asset('js/selectize.min.js') }}"></script> 
+
+<script src="{{ asset('js/custom.js') }}"></script>
+<script src="{{ asset('js/ckeditor/ckeditor.js') }}"></script>
+<!-- SHORTCUT JS -->
+<script src="{{ asset('js/shortcut.js') }}"></script>
+
+
+@yield('scripts')
+
+</html>
---
+++ resources/views/layouts/app_login.blade.php
@@ -0,0 +1,197 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/apple-icon.png') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+        <title>Thasya Busana</title>
+
+      <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Bootstrap core CSS     -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
+    <!--  Material Dashboard CSS    -->
+    <link href="{{ asset('css/material-kit.css?v=1.2.0') }}" rel="stylesheet" />
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+
+     <link href="{{ asset('css/selectize.bootstrap3.css') }}" rel="stylesheet">
+
+     
+    <link href="{{ asset('css/bootstrap-datepicker.min.css') }}" rel="stylesheet">
+     
+    <!--     Fonts and icons     -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+  <style type="text/css">
+    
+    .table>thead>tr>th, .table>tbody>tr>th, .table>tfoot>tr>th, .table>thead>tr>td, .table>tbody>tr>td, .table>tfoot>tr>td{
+    padding: 1px;
+    }
+  </style>
+    <style>
+      #map {
+        height: 400px;
+        width: 150%;
+       }
+    </style>
+
+<body class="blog-posts">
+
+    <nav class="navbar navbar-primary navbar-transparent navbar-absolute">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand">Thasya Busana</a>
+            </div>
+
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav navbar-right"> 
+                    <li>
+                        <a href="{{ url('/login') }}">
+                            <i class="material-icons">fingerprint</i> Login
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ url('/register') }}">
+                            <i class="material-icons">person_add</i> Register
+                        </a>
+                    </li>
+  
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="page-header header-filter header-small" data-parallax="true" style="background-image: url('image/logo.jpg');"> 
+    </div>
+
+    <div class="main main-raised">
+        <div class="container">
+                    @yield('content')
+        </div>
+    </div>
+ 
+
+        <div class="team-5 section-image" style="background-image: url('../assets/img/bg10.jpg')">
+
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-6">
+                        <div class="card card-profile card-plain">  
+                                <div class="card-content">
+                                    <h4 class="card-title">Thasya Busana</h4>
+                                    <h6 class="category text-muted">____________</h6>
+
+                                    <p class="card-description">
+                                        Don't be scared of the truth because we need to restart the human foundation in truth...
+                                    </p>
+
+                                    <div class="footer">
+                                        <a href="#pablo" class="btn btn-just-icon btn-simple btn-white"><i class="fa fa-twitter"></i></a>
+                                        <a href="#pablo" class="btn btn-just-icon btn-simple btn-white"><i class="fa fa-facebook-square"></i></a>
+                                        <a href="#pablo" class="btn btn-just-icon btn-simple btn-white"><i class="fa fa-google"></i></a>
+                                    </div>
+                                </div> 
+                        </div>
+                    </div>
+
+                    <div class="col-md-5">
+                        <div class="card card-profile card-plain"> 
+                                  <div id="map"></div> 
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+ 
+    </div>
+ 
+
+<!--   Core JS Files   -->
+<script src="{{ asset('js/jquery-3.2.1.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/material.min.js') }}" type="text/javascript"></script>
+
+<script src="{{ asset('js/bootstrap-datepicker.min.js') }}"></script>
+<script src="{{ asset('js/perfect-scrollbar.jquery.min.js') }}" type="text/javascript"></script>
+<!-- Library for adding dinamically elements -->
+<script src="{{ asset('js/arrive.min.js') }}" type="text/javascript"></script>
+<!-- Forms Validations Plugin -->
+<script src="{{ asset('js/jquery.validate.min.js') }}"></script>
+<!-- Promise Library for SweetAlert2 working on IE -->
+<script src="{{ asset('js/es6-promise-auto.min.js') }}"></script>
+<!--  Plugin for Date Time Picker and Full Calendar Plugin-->
+<script src="{{ asset('js/moment.min.js') }}"></script>
+<!--  Charts Plugin, full documentation here: https://gionkunz.github.io/chartist-js/ -->
+<script src="{{ asset('js/chartist.min.js') }}"></script>
+<!--  Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
+<script src="{{ asset('js/jquery.bootstrap-wizard.js') }}"></script>
+<!--  Notifications Plugin, full documentation here: http://bootstrap-notify.remabledesigns.com/    -->
+<script src="{{ asset('js/bootstrap-notify.js') }}"></script>
+<!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
+<script src="{{ asset('js/bootstrap-datetimepicker.js') }}"></script>
+<!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
+<script src="{{ asset('js/jquery-jvectormap.js') }}"></script>
+<!-- Sliders Plugin, full documentation here: https://refreshless.com/nouislider/ -->
+<script src="{{ asset('js/nouislider.min.js') }}"></script>
+<!--  Google Maps Plugin    -->
+<!-- <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> -->
+<!--  Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+<script src="{{ asset('js/jquery.select-bootstrap.js') }}"></script>
+<!--  DataTables.net Plugin, full documentation here: https://datatables.net/    -->
+<script src="{{ asset('js/jquery.dataTables.js') }}"></script>
+<!-- Sweet Alert 2 plugin, full documentation here: https://limonte.github.io/sweetalert2/ -->
+<script src="{{ asset('js/sweetalert2.js') }}"></script>
+<!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+<script src="{{ asset('js/jasny-bootstrap.min.js') }}"></script>
+<!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
+<script src="{{ asset('js/fullcalendar.min.js') }}"></script>
+<!-- Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
+<script src="{{ asset('js/jquery.tagsinput.js') }}"></script>
+<!-- Material Dashboard javascript methods -->
+<script src="{{ asset('js/material-dashboard.js?v=1.2.0') }}"></script>
+<!-- Material Dashboard DEMO methods, don't include it in your project! -->
+<script src="{{ asset('js/demo.js') }}"></script>
+
+<script src="{{ asset('js/selectize.min.js') }}"></script> 
+
+<script src="{{ asset('js/custom.js') }}"></script>
+<script src="{{ asset('js/ckeditor/ckeditor.js') }}"></script>
+<!-- SHORTCUT JS -->
+<script src="{{ asset('js/shortcut.js') }}"></script>
+
+    <script>
+      function initMap() {
+        var uluru = {lat: -5.3791173, lng: 105.2412983};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 16,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDUfTCdFiOgiKp6LSPAVfIZL1r3NE-dJ2Y&callback=initMap">
+    </script>@yield('scripts')
+ 
+
+</html>
